568203282926b881b608c7edc8e1fd458a47a869
arid0910 <arid0910@g.educaand.es>
2023-12-01 08:55:07 +0100

    Inico del proyecto

---
+++ src/main/java/adamridings/pruebapushpull/PruebaPushPull.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package adamridings.pruebapushpull;
+
+/**
+ *
+ * @author adam
+ */
+public class PruebaPushPull {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
